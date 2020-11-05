aa8559330166b53a71e72aaf0be393d0cd497dc0
James Sizeland <jamessizeland@gmail.com>
2020-11-05 21:39:13 +0000

    completes post service

--- posts/index.js
+++ posts/index.js
@@ -2,19 +2,33 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 // initialize app variables
+const {
+    randomBytes
+} = require('crypto');
 const port = 4000;
+const posts = {};
 
-
-// handle browser requests
+// handle service requests for data
 app.get("/posts", (req, res) => {
-
+    res.send(posts);
 });
 
+// handle service incoming data
 app.post("/posts", (req, res) => {
-
+    const id = randomBytes(4).toString('hex'); //randomly assign id
+    const {
+        title
+    } = req.body;
+    posts[id] = {
+        id,
+        title
+    };
+    res.status(201).send(posts[id]);
 });
 
 //spin up server
