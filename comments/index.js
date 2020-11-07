91ad5b292e16b11347503a773ea33cd92731c3c9
James Sizeland <jamessizeland@gmail.com>
2020-11-07 23:47:30 +0000

    Adds comment moderated business logic to comments service.

--- comments/index.js
+++ comments/index.js
@@ -41,8 +41,21 @@ app.post("/posts/:id/comments", async (req, res) => {
     res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
     console.log('Received Event:', req.body.type);
+    const {type, data} = req.body;
+
+    if (type === 'CommentModerated') {
+        const {postId, id, status, content} = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => comment.id === id)
+        comment.status = status;
+
+        await axios.post(`http://localhost:${eventServicePort}/events`, {
+            type: 'CommentUpdated',
+            data: {id, status, postId, content}
+        });
+    }
     res.send({status: 'OK'}); // respond to event with empty object to confirm
 });
 
