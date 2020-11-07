5ab385685bc13cc8a2c8062c1f81f0f8e7a8ffa9
James Sizeland <jamessizeland@gmail.com>
2020-11-07 23:28:17 +0000

    Adds comment status field to comments and query services.

--- comments/index.js
+++ comments/index.js
@@ -23,10 +23,9 @@ app.post("/posts/:id/comments", async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const {content} = req.body;
     const comments = commentsByPostId[req.params.id] || []; //if no comments yet
-    comments.push({
-        id: commentId,
-        content
-    });
+    
+    // TODO this looks like there's some duplication of code?
+    comments.push({id: commentId, content, status:'pending'});
     commentsByPostId[req.params.id] = comments;
 
     await axios.post(`http://localhost:${eventServicePort}/events`, {
@@ -34,7 +33,8 @@ app.post("/posts/:id/comments", async (req, res) => {
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     });
 
