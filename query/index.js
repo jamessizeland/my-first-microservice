c3e1f1934a9dac8b072823d4b34bcb76552b9391
James Sizeland <jamessizeland@gmail.com>
2020-11-08 11:28:30 +0000

    adds missing axios package to query service.

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 // initialize node packages
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
