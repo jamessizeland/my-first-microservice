6aefa3fcec22b267e889034adead6e772a08aeed
James Sizeland <jamessizeland@gmail.com>
2020-11-08 10:46:03 +0000

    updates react UI to reflect moderation status.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,7 +3,19 @@ import React from 'react';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ( { comments } ) => {
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        const status = comment.status;
+        let content;
+        if (status === 'approved') {
+            content = comment.content;
+        } else if (status === 'pending') {
+            content = 'This comment is awaiting moderation';
+        } else if (status === 'rejected') {
+            content = 'This comment has been rejected';
+        } else {
+            content = 'Unexpected status';
+        }
+
+        return <li key={comment.id}>{content}</li>
     });
 
     return <ul>{renderedComments}</ul>;
